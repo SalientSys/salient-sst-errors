a3f6f691445b5a79308a84cdcaf2e530bf1aa285
Sohail Haqyar <sohail.haqyar@salientsys.com>
2022-12-06 20:57:24 -0600

    more docs

--- src/error-code-generator.ts
+++ src/error-code-generator.ts
@@ -1,6 +1,11 @@
 import { routeMap } from './routes-map';
 import { Middleware, RouteName, Service } from './enums';
 
+/**
+ *         Route Middleware Service
+ * @returns 100  10000      10000   // 13 digit error code that can be parsed using the parseErrorCode function in this package
+ *
+ */
 const generateErrorCode = (
   route: number,
   middleware: Middleware,
