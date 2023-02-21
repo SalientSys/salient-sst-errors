ce11104aab2921d69506ca858956890b8dcb10f6
Sohail Haqyar <sohail.haqyar@salientsys.com>
2023-02-21 08:03:34 -0600

    made route and middleware optional for more flexibility

--- src/interfaces/error-context.interface.ts
+++ src/interfaces/error-context.interface.ts
@@ -1,7 +1,7 @@
 import { Middleware, RouteName, Service } from '../enums';
 
 export interface IErrorContext {
-  route: RouteName;
-  middleware: Middleware;
+  route?: RouteName;
+  middleware?: Middleware;
   service?: Service;
 }
