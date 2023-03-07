57ba3004b5f2caf2239472ba5a7d92d3fe13333c
Sohail Haqyar <sohail.haqyar@salientsys.com>
2023-03-07 16:41:02 -0600

    removed unused code

--- src/error-code-generator.ts
+++ src/error-code-generator.ts
@@ -1,7 +1,5 @@
 import { routeMap } from './routes-map';
-import { DefaultErrorInfo, Details, Middleware, RouteName } from './enums';
-import { IErrorContext } from './interfaces';
-
+import { Middleware, RouteName } from './enums';
 /**
  * @returns Route: XXX  Middlware: XXXXX  Details:XXXXX
  *   Can be parsed using the parseErrorCode function
@@ -16,16 +14,4 @@ const generateErrorCode = (
 
 const generateErrorOriginCode = (route: RouteName) => routeMap[route];
 
-function getErrorCodeFromContext(context: IErrorContext) {
-  const routeCode = routeMap[context.route];
-  const detailsCode = Details[context.details];
-
-  const code = generateErrorCode(
-    routeCode ?? DefaultErrorInfo.DefaultRoute,
-    context.middleware,
-    detailsCode,
-  );
-
-  return code;
-}
-export { generateErrorCode, generateErrorOriginCode, getErrorCodeFromContext };
+export { generateErrorCode, generateErrorOriginCode };
