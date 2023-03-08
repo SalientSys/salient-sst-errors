fcd64a591c8a9413e859f505f0c070d43f7a7072
Ahmad Sohail Haqyar <sohail.haqyar@salientsys.com>
2023-03-08 09:54:06 -0800

    Update auth.ts with better friendly messages

--- src/error-details/auth.ts
+++ src/error-details/auth.ts
@@ -2,24 +2,24 @@ import { Details } from '..';
 import { IDetailsMap } from '../interfaces';
 export const authErrorDetails: IDetailsMap = {
   [Details.NoTokenFound]: {
-    friendlyMessage: 'No token found',
+    friendlyMessage: 'Unable to authenticate request',
     technichalReason:
       'Could not get the token from the req.headers.authorization',
   },
 
   [Details.NoTokenSecret]: {
-    friendlyMessage: 'No token secret found',
+    friendlyMessage: 'Unable to authenticate request',
     technichalReason:
       'Could not get the token secret from the process.env.TOKEN_SECRET for token signing',
   },
 
   [Details.JwtVerifyFailed]: {
-    friendlyMessage: 'Bad token',
+    friendlyMessage: 'Unable to authenticate request',
     technichalReason: 'jwt.verify failed to verify the token, and errored out',
   },
 
   [Details.NoTokenAudience]: {
-    friendlyMessage: 'Bad token',
+    friendlyMessage: 'Unable to authenticate request',
     technichalReason: '(in jwt.verify) The token.aud returned a falsy value',
   },
 };
